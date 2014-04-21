8ae7b6e1edfb7992b348e9eb2640da3c76faf607
Tom <tborychowski@gmail.com>
2014-04-21 15:02:51 +0100

    symlink > copy

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ shopt -s nullglob
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for f in *.js
 do
-	cp $DIR/$f $HOME/bin/${f%.*}
+	ln -s $DIR/$f $HOME/bin/${f%.*}
 	echo -e "\e[32m[OK]\e[39m ${f%.*}"
 
 	# cp $f /usr/local/bin/${f%.*}
+	# cp $DIR/$f $HOME/bin/${f%.*}
 	# sed -i '1i#!/usr/bin/node' /usr/local/bin/${f%.*}
 done
